bf1c7ef0fe9ab62a04f1eb1595ac1323b2258610
Qwerty042 <28468486+Qwerty042@users.noreply.github.com>
2025-01-25 15:45:20 +1300

    add parsing of level data and permutation data

--- Assets/Scripts/TileManager.cs
+++ Assets/Scripts/TileManager.cs
@@ -4,7 +4,9 @@ using System.Collections.Generic;
 using System.IO;
 using Microsoft.Unity.VisualStudio.Editor;
 using Unity.Mathematics;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class TileManager : MonoBehaviour
 {
@@ -16,51 +18,77 @@ public class TileManager : MonoBehaviour
 
     private enum GameState
     {
+        gsPRE_START,
+        gsLOAD_LEVEL,
         gsSELECTING,
-        gsSWAPPING
+        gsSWAPPING,
+        gsCHECK_ORDER
     }
 
-    // private struct Tile //TODO: move to this struct?
-    // {
-    //     GameObject tileObject;
-    //     int goalIndex;
-    //     int currentIndex;
-    // }
+    private struct OrderPermutation
+    {
+        public int[] order;
+        public int minSwaps;
+        public int bubbleSwaps;
+    }
 
-    private GameState gameState = GameState.gsSELECTING;
+    private struct LevelMetadata
+    {
+        public int level;
+        public string main_title;
+        public string left_title;
+        public string right_title;
+    }
 
-    private GameObject[] tiles;
+    private GameState gameState = GameState.gsPRE_START;
+
+    private int nextLevel = 0;
+    private List<OrderPermutation> orderPermutations = new List<OrderPermutation>();
+    private List<LevelMetadata> levelMetadatas = new List<LevelMetadata>();
+    private GameObject[] tiles = new GameObject[7];
     private GameObject[] tileSelectBoarders = new GameObject[2] {null, null};
     private GameObject[] swapTiles = new GameObject[2] {null, null};
     private Vector3[] tileDestinations = new Vector3[2];
     private bool doSwap = false;
-    private int NUM_TILES;
     private const float SWAP_MOVE_TIME = 0.25f;
     private const float SWAP_AMPLITUDE = 2f;
     private float swapTimer = 0f;
-    // private const int NUM_PERMUTATIONS = 5018;
 
     void Start()
-    {        
-        NUM_TILES = tileSpots.Length;
-        Sprite sp0 = Resources.Load<Sprite>("Levels/0-0");
-        Sprite sp1 = Resources.Load<Sprite>("Levels/0-1");
-        Sprite sp2 = Resources.Load<Sprite>("Levels/0-2");
-        tiles = new GameObject[NUM_TILES];
-        for (int i = 0; i < NUM_TILES; i++)
+    {
+        for (int i = 0; i < tileSpots.Length; i++)
         {
-            tiles[i] = Instantiate(tilePrefab, tileSpots[i], Quaternion.identity);
+            tiles[i] = Instantiate(tileSelectBoarderPrefab, tileSpots[i], Quaternion.identity);
         }
-        Debug.Log("Hello World!");
-        tiles[0].GetComponent<SpriteRenderer>().sprite = sp0;
-        tiles[1].GetComponent<SpriteRenderer>().sprite = sp1;
-        tiles[2].GetComponent<SpriteRenderer>().sprite = sp2;
+
+        LoadPermutations();
+        LoadLevelMetadata();
+
+        // Sprite sp0 = Resources.Load<Sprite>("Levels/0-0");
+        // Sprite sp1 = Resources.Load<Sprite>("Levels/0-1");
+        // Sprite sp2 = Resources.Load<Sprite>("Levels/0-2");
+
+        // Debug.Log("Hello World!");
+        // tiles[0].GetComponent<SpriteRenderer>().sprite = sp0;
+        // tiles[0].GetComponent<TileProperties>().correctIndex = 6;
+        // tiles[1].GetComponent<SpriteRenderer>().sprite = sp1;
+        // tiles[2].GetComponent<SpriteRenderer>().sprite = sp2;
     }
 
     void Update()
     {
         switch (gameState)
         {
+            case GameState.gsPRE_START:
+                if(Input.GetKeyDown(KeyCode.Space))
+                {
+                    gameState = GameState.gsLOAD_LEVEL;
+                }
+                break;
+            case GameState.gsLOAD_LEVEL:
+                LoadLevel(nextLevel);
+                nextLevel++;
+                break;
             case GameState.gsSELECTING:
                 CheckSwapping();
                 if(doSwap)
@@ -75,10 +103,52 @@ public class TileManager : MonoBehaviour
                 if (!doSwap)
                 {
                     ClearTileSelection();
-                    gameState = GameState.gsSELECTING;
+                    gameState = GameState.gsCHECK_ORDER;
                 }
                 break;
         }
+    }
+
+    void LoadPermutations()
+    {
+        StreamReader fp = new StreamReader("Assets/Resources/Levels/all_permutations.csv");
+        fp.ReadLine(); // Ignore first line which has the column titles
+        string line = fp.ReadLine();
+        while (line != null)
+        {
+            OrderPermutation permutation = new OrderPermutation();
+            string[] cols = line.Split(';');
+            permutation.order = Array.ConvertAll(cols[0].Split(','), int.Parse);
+            permutation.minSwaps = int.Parse(cols[1]);
+            permutation.bubbleSwaps = int.Parse(cols[2]);
+            orderPermutations.Add(permutation);
+            line = fp.ReadLine();
+        }
+        // OrderPermutation permutation1 = orderPermutations[3000];
+        // Debug.Log($"{permutation1.order[0]},{permutation1.order[1]},{permutation1.order[2]},{permutation1.order[3]},{permutation1.order[4]},{permutation1.order[5]},{permutation1.order[6]} - {permutation1.minSwaps} - {permutation1.bubbleSwaps}");
+    }
+
+    void LoadLevelMetadata()
+    {
+        StreamReader fp = new StreamReader("Assets/Resources/Levels/levels.csv");
+        string line = fp.ReadLine();
+        while (line != null)
+        {
+            LevelMetadata levelMetadata = new LevelMetadata();
+            string[] cols = line.Split(',');
+            levelMetadata.level = int.Parse(cols[0]);
+            levelMetadata.main_title = cols[1];
+            levelMetadata.left_title = cols[2];
+            levelMetadata.right_title = cols[3];
+            levelMetadatas.Add(levelMetadata);
+            line = fp.ReadLine();
+            Debug.Log($"{levelMetadata.level} - {levelMetadata.main_title} - {levelMetadata.left_title} - {levelMetadata.right_title}");            
+        }
+    }
+
+    void LoadLevel(int level)
+    {
+        
     }
 
     void CheckSwapping()
@@ -101,7 +171,6 @@ public class TileManager : MonoBehaviour
                 {
                     swapTiles[0] = hit.transform.gameObject;
                     tileSelectBoarders[0] = Instantiate(tileSelectBoarderPrefab, swapTiles[0].transform.position, Quaternion.identity);
-                    sfx.Play();
                 }
                 else if (swapTiles[0].GetInstanceID() == hit.transform.gameObject.GetInstanceID()) // Tile is already selected, deselect:
                 {
